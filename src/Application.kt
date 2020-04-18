4d80e9b712e4b9591f1c38eeca07489d37540123
sorting-mjatt <9652156+sorting-mjatt@users.noreply.github.com>
2020-04-18 11:30:06 +1000

    Ktor generated boilerplate

---
+++ src/Application.kt
@@ -0,0 +1,21 @@
+package jib
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/health") {
+            call.respondText("green", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
